27e0ee27835d6e30194887a3e2acd1441a263557
Niklas Andersson <niklas.andersson@openforce.se>
2019-01-30 01:49:19 +0100

    Added local_settings.py

---
+++ playbooks/files/local_settings.py
@@ -0,0 +1,12 @@
+DEBUG=False
+SECRET_KEY = 'q96ek8frocksf9)&-wf*cu9wqblackbird(@h#_ze3%@@n*(c(2^dr2du8_9ctf%'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'freemarketmalaga',
+        'USER': 'django',
+        'PASSWORD': 'Secret11',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
